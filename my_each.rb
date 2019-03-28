4820cbda7c67f3772e225e03805aa05d108adc9f
minh nguyen <imnotminh@gmail.com>
2019-03-28 22:33:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  
+  i=0  
+  new_array=[]
+  
+  while i<array.length
+    yield (array[i])
+    new_array.push(array[i])
+    i+=1
+  end
+  new_array
+
 end
